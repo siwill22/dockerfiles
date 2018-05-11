c4b8f1d78d8441dcd5c476adf894637cb42a4833
siwill22 <siwill22@gmail.com>
2018-05-11 11:13:12 +1000

    change name to lab; add missing dependencies; add sample data; avoid password

---
+++ pygplates-lab/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#ipcluster start --profile=mpi &
+jupyter lab --ip=* --port=8889 --no-browser --allow-root
+
--- pygplates-workspace/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ipcluster start --profile=mpi &
-jupyter lab --ip=0.0.0.0 --no-browser
-
